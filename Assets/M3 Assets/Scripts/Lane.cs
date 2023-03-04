b799e6cb6fb60be990abf8da613864cc06a1f174
tobiglaser <76131623+tobiglaser@users.noreply.github.com>
2023-03-05 00:39:28 +0100

    Lane Wrapper for SpawnPoints

---
+++ Assets/M3 Assets/Scripts/Lane.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lane : MonoBehaviour
+{
+    public readonly Transform RightSpawn;
+    public readonly Transform LeftSpawn;
+}
