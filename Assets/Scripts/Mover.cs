66299acf699112384228d3e73b226c0e254eeee3
tobiglaser <76131623+tobiglaser@users.noreply.github.com>
2023-03-04 13:42:29 +0100

    able to drop Vehicle at legal position

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -7,7 +7,9 @@ public class Mover : MonoBehaviour
 {
 
     [SerializeField] private GameObject shape;
+    [SerializeField] private Vector3 LiftingOffset = Vector3.up;
     private GameObject grabbedObj = null;
+    Vector3 originalPos;
     private Mesh initialMesh;
 
     // Start is called before the first frame update
@@ -19,40 +21,79 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //grab
         if (Input.GetMouseButtonDown(0))
         {
-            grabbedObj = getClickedGameObject();
-            if (grabbedObj != null)
-            {
-                grabbedObj.transform.Translate(Vector3.up);
-                Mesh mesh = grabbedObj.GetComponent<MeshFilter>().mesh;
-                shape.GetComponent<MeshFilter>().mesh = mesh;
-            }
+            grabObject();
         }
 
-        //release
-        if (Input.GetMouseButtonUp(0) && grabbedObj != null)
+        if (Input.GetMouseButtonUp(0))
         {
-            Vector3? newPos = getGroundUnderCursor();
-            if (newPos == null)
-            {
-                // lets go
-            }
-
-            grabbedObj.transform.Translate(Vector3.down);
-            shape.GetComponent<MeshFilter>().mesh = initialMesh;
-            grabbedObj = null;
+            releaseObject();
         }
 
-        //move with cursor
         if (grabbedObj != null)
         {
-            Vector3? groundPoint = getGroundUnderCursor();
-            if (groundPoint != null)
+            moveWithCursor();
+        }
+    }
+
+    private void moveWithCursor()
+    {
+        Vector3? groundPoint = getGroundUnderCursor();
+        if (groundPoint.HasValue)
+        {
+            shape.transform.position = groundPoint.Value + new Vector3(0, originalPos.y + LiftingOffset.y, 0);
+        }
+    }
+
+    private void releaseObject()
+    {
+        Vector3? cursorPos = getGroundUnderCursor();
+        if (grabbedObj == null || cursorPos == null)
+            return;
+        Vector3 releasePos;
+        if (isLegal(cursorPos))
+        {
+            releasePos = new Vector3(cursorPos.Value.x, cursorPos.Value.y + originalPos.y, cursorPos.Value.z);
+        }
+        else
+        {
+            releasePos = originalPos;
+        }
+
+        grabbedObj.transform.position = releasePos;
+
+        shape.GetComponent<MeshFilter>().mesh = initialMesh;
+        grabbedObj = null;
+    }
+
+    private bool isLegal(Vector3? position)
+    {
+        if (position.HasValue)
+        {
+            Ray ray = new Ray(position.Value + (10 * Vector3.up), Vector3.down);
+            RaycastHit[] hits = Physics.RaycastAll(ray);
+            for (int i = 0; i < hits.Length; i++)
             {
-                shape.transform.position = (Vector3)groundPoint + new Vector3(0, grabbedObj.transform.position.y, 0);
+                if (hits[i].collider.CompareTag("DropArea"))
+                {
+                    return true;
+                }
             }
+        }
+        return false;
+    }
+
+
+    private void grabObject()
+    {
+        grabbedObj = getClickedGameObject();
+        if (grabbedObj != null)
+        {
+            originalPos = grabbedObj.transform.position;
+            grabbedObj.transform.Translate(LiftingOffset);
+            Mesh mesh = grabbedObj.GetComponent<MeshFilter>().mesh;
+            shape.GetComponent<MeshFilter>().mesh = mesh;
         }
     }
 
@@ -68,7 +109,7 @@ public class Mover : MonoBehaviour
         {
             return null;
         }
-        if (!hit.collider.CompareTag("Ground"))
+        if (hit.collider.CompareTag("Vehicle"))
         {
             return hit.transform.gameObject;
         }
